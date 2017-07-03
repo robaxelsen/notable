6bf4f03a5e43a3edf0a4b5057b4926e5bba14b78
robeerob <rob@rob.ee>
2017-07-03 22:11:12 +0200

    Add cors package for dev purposes
    Add cors package to enable easy setup of the react part while I get
    more comfortable with both react, and later webpack.

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
+var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Using cors package to enable cors requests temporarily while
+// experimenting with react in a different project. Will set up
+// this properly once I am more comfortable with webpack etc
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => {
