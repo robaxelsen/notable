2351e5d656875d3d5ee3e465edbc4ba71a2f47a4
robeerob <rob@rob.ee>
2017-07-13 20:51:49 +0200

    Use classes instead of createElement

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
+class NoteTitle extends React.Component {
+  onClick() {
+    console.log('I was clicked', this.props.name);
+  }
+  render(){
+    return (
+      <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+    )
+  }
+}
+
+class NoteTitleList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.notes.map( note => {
+          return (
+            <NoteTitle name={note.name} />
+          )
+        }
+      )}
+      </ul>
+    )
+  }
+}
+
 function requestListener () {
   var notes = [JSON.parse(this.responseText)][0];
-  ReactDOM.render(
-    React.createElement('ul', { className: "notes" },
-      notes.map((note, i) =>
-        React.createElement('li', { key: i }, note.title)
-      )
-    ),
-    document.getElementById('notes')
-  )
+  ReactDOM.render(<NoteTitleList titles={notes} />,
+    document.getElementById('app'));
+  //ReactDOM.render(
+  //  React.createElement('ul', { className: "notes" },
+  //    notes.map((note, i) =>
+  //      React.createElement('li', { key: i }, note.title)
+  //    )
+  //  ),
+  //  document.getElementById('notes')
+  //)
 };
 
 var oReq = new XMLHttpRequest();
