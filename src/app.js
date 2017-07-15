bf9d39697aaab7a5a542e233f258b74c4e914ace
robeerob <rob@rob.ee>
2017-07-15 08:10:39 +0200

    Fix broken React components

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-class NoteTitle extends React.Component {
+class Note extends React.Component{
   onClick() {
-    console.log('I was clicked', this.props.name);
+    console.log('I was clicked', this.props.title);
   }
-  render(){
+  render() {
     return (
-      <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+      <li onClick={this.onClick.bind(this)}>{this.props.title}</li>
     )
   }
 }
 
-class NoteTitleList extends React.Component {
+class NoteList extends React.Component{
   render() {
     return (
       <ul>
         {this.props.notes.map( note => {
-          return (
-            <NoteTitle name={note.name} />
-          )
-        }
-      )}
+            return (
+              <Note title={note.title} />
+            )
+          }
+        )}
       </ul>
     )
   }
@@ -26,16 +26,8 @@ class NoteTitleList extends React.Component {
 
 function requestListener () {
   var notes = [JSON.parse(this.responseText)][0];
-  ReactDOM.render(<NoteTitleList titles={notes} />,
+  ReactDOM.render(<NoteList notes={notes} />,
     document.getElementById('app'));
-  //ReactDOM.render(
-  //  React.createElement('ul', { className: "notes" },
-  //    notes.map((note, i) =>
-  //      React.createElement('li', { key: i }, note.title)
-  //    )
-  //  ),
-  //  document.getElementById('notes')
-  //)
 };
 
 var oReq = new XMLHttpRequest();
